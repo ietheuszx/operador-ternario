f313d05a409a50d0ee41b79a1403c95bb09dae32
ietheuszx <fernandomaranss68@gmail.com>
2024-04-10 16:23:32 -0400

    resolvida questao 9

---
+++ questao9.js
@@ -0,0 +1,10 @@
+/*
+Questão 9: Escreva um código JavaScript utilizando o operador ternário para 
+determinar se um ano é bissexto. A variável ano contém o ano a ser verificado. 
+Atribua o resultado à variável resultado.
+*/
+
+let ano = 2000;
+let condissao = (ano % 4 == 0) ? "Ano bicesso" : "Ano normar"
+console.log(condissao)
+    
